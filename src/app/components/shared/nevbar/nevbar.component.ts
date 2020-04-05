32586556041e6cfd55443259aa075a59eecc80b9
jemc300x <jemc300x@gmail.com>
2020-04-05 07:55:05 -0500

    components home, search, artist and navbar created

---
+++ src/app/components/shared/nevbar/nevbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nevbar',
+  templateUrl: './nevbar.component.html',
+  styles: []
+})
+export class NevbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
